6c56bf054a715db1f9b2782b9e148d5e9c82976c
jon175 <jonpapaye13@gmail.com>
2017-01-11 23:16:11 +0800

    優化4

--- js/script-上傳.js
+++ js/script-上傳.js
@@ -1,39 +1,42 @@
-// $(document).ready(function(){
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyCnyVc25JVedpfLpKq41DwZqwxxkIjzEfM",
-//     authDomain: "ntue-backstage.firebaseapp.com",
-//     databaseURL: "https://ntue-backstage.firebaseio.com",
-//     storageBucket: "ntue-backstage.appspot.com",
-//     messagingSenderId: "205231721291"
-//   };
-//   firebase.initializeApp(config);
-//
-// var storageRef = firebase.storage().ref();
-//
-// const $file = $('#homework');
-// const $btnload = $('#btnload');
-//
-// // Load
-// $btnSubmit.click(function(){
-//   var user = firebase.auth().currentUser;
-//   const $userName = $('#userName').val();
-//   const userName = $('#userName').val();
-//
-//   const promise = user.updateProfile({
-//     displayName: $userName,
-//     photoURL: photoURL
-//   });
-//   promise.then(function() {
-//
-//     console.log("Update successful.");
-//     user = firebase.auth().currentUser;
-//     if (user) {
-//       console.log(user.uid);
-//        $img.attr("src",user.photoURL);
-//     };
-//     window.location.href = "./作業區.html";
-//
-//   });
-// });
-// });
+$(document).ready(function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCnyVc25JVedpfLpKq41DwZqwxxkIjzEfM",
+    authDomain: "ntue-backstage.firebaseapp.com",
+    databaseURL: "https://ntue-backstage.firebaseio.com",
+    storageBucket: "ntue-backstage.appspot.com",
+    messagingSenderId: "205231721291"
+  };
+  firebase.initializeApp(config);
+
+    //Get Elementary
+    var uploader =document.getElementsById('uploader');
+    var fileButton =document.getElementsById('fileButton');
+
+    //listen for file selection
+          fileButton.addEventListener('change',function(e){
+            //get file
+          var file=e.target.files[0];
+          //create a storage ref
+          var storageRef=firebase.storage().ref('data'+file.name);
+          //upload file
+          var task=storageRef.put(file);
+          //upload progress bar
+          task.on('state_charged',
+
+                function progress(snapshot){
+                  var percentage=(snapshot.bytesTransferred/
+                    snapshot.totalBytes)*100;
+                    uploader.value=percentage;
+                },
+
+                function error(err){
+                },
+
+                function complete(){
+
+                }
+         );
+      });
+
+});
